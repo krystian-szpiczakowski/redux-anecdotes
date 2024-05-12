1eba906d9e734e047373fbb6407a0e030c2d491a
Krystian Szpiczakowski <szpiczakowski.krystian@gmail.com>
2024-05-12 15:39:40 +0200

    Part 6.3 - Implement reducer with tests and React component

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -23,7 +23,16 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch(action.type) {
+    case 'VOTE':
+      const id = action.payload.id
+      const anecdoteToChange = state.find(anecdote => anecdote.id === id)
+      console.log("Found anecdote: " + anecdoteToChange)
+      const changedAnecdote = {...anecdoteToChange, votes: anecdoteToChange.votes + 1}
+      const updatedState = state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+      return updatedState
+    default: return state
+  }
 }
 
 export default reducer
---
+++ src/test/anecdoteReducer.test.js
@@ -0,0 +1,70 @@
+import reducer from "../reducers/anecdoteReducer"
+import deepFreeze from "deep-freeze"
+
+describe('storing votes', () => {
+    const initialState = [
+        {
+            id: 1,
+            content: "Lorem ipsum",
+            votes: 0
+        },
+        {
+            id: 2,
+            content: "Ipsum lorem",
+            votes: 0
+        },
+        {
+            id: 3,
+            content: "Oh my!",
+            votes: 0
+        }
+    ]
+
+    test('all anecdotes have 0 votes at the beginning', () => {
+        var action = {
+            type: "DO_NOTHING"
+        }
+
+        const initialState = reducer(undefined, action)
+        expect(initialState).toHaveLength(6)
+        initialState.forEach(anecdote => expect(anecdote.votes).toBe(0))
+    })
+
+    test('single vote increments votes only for one anecdote', () => {
+        const selectedId = 1
+
+        var action = {
+            type: "VOTE",
+            payload: {
+                id: selectedId
+            }
+        }
+
+        deepFreeze(initialState)
+
+        const state = reducer(initialState, action)
+        const votedAnecdote = state.find(item => item.id === selectedId)
+        expect(votedAnecdote.votes).toBe(1)
+        
+        const notVotedAnecdotes = state.filter(item => item.id !== selectedId)
+        notVotedAnecdotes.forEach(anecdote => expect(anecdote.votes).toBe(0))
+    })
+
+    test('voting the same anecdote multiple times increments counter accordingly', () => {
+        const selectedId = 2
+
+        var action = {
+            type: "VOTE",
+            payload: {
+                id: selectedId
+            }
+        }
+
+        deepFreeze(initialState)
+
+        let state = reducer(initialState, action)
+        state = reducer(state, action)
+        const votedAnecdote = state.find(item => item.id === selectedId)
+        expect(votedAnecdote.votes).toBe(2)
+    })
+})
