9e1f967add8ebecc429bf4c2de072d7396da8b9a
Krystian Szpiczakowski <szpiczakowski.krystian@gmail.com>
2024-05-23 09:53:17 +0200

    Part 6.14 - Fetch anecdotes from db.json

--- src/components/AnecdoteList.jsx
+++ src/components/AnecdoteList.jsx
@@ -1,15 +1,24 @@
 import { useDispatch, useSelector } from "react-redux"
-import { vote } from "../reducers/anecdoteReducer"
-import { setNotification, clearNotification } from "../reducers/notificationReducer"
+import { vote, setAnecdotes } from "../reducers/anecdoteReducer"
+import { setNotification } from "../reducers/notificationReducer"
+import anecdoteService from '../services/anecdotes'
+import { useEffect } from "react"
 
 const AnecdoteList = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        anecdoteService.getAll().then(items => {
+            dispatch(setAnecdotes(items))
+        })
+    }, [])
+
     const anecdotes = useSelector(state => state.anecdotes)
     const filterContent = useSelector(state => state.filter.content)
     
     const anecdotesByVotes = anecdotes.slice().sort((a, b) => b.votes - a.votes)
     const filteredAndSorted = anecdotesByVotes.filter(anecdote => anecdote.content?.toLowerCase().includes(filterContent?.toLowerCase()))
 
-    const dispatch = useDispatch()
 
     const handleVote = (id) => {
         dispatch(vote({id}))
